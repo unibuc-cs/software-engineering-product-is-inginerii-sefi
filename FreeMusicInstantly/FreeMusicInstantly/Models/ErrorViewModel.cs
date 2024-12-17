ad63cbc19dea016b492aad1d46a618d2e0164fb1
Apostol Alin-Constantin <115925095+Apostol-Alin@users.noreply.github.com>
2024-12-17 20:08:44 +0200

    Revert "Users and roles (#4)"
    This reverts commit 0f1227845f6b0a58671e6b7aed6a095a555bc84c.

--- FreeMusicInstantly/FreeMusicInstantly/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FreeMusicInstantly.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
