275738a727f7eb88bc4f6b75d8adc0b6ba9ca511
Pirvulescu Daria <dariapirvulescu@yahoo.ro>
2024-12-31 10:39:21 +0200

    Added the functinality of the feature album

---
+++ FreeMusicInstantly/FreeMusicInstantly/Models/SongAlbum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreeMusicInstantly.Models
+{
+    public class SongAlbum
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int? SongId { get; set; }
+        public int? AlbumId { get; set; }
+        public virtual Song? Song { get; set; }
+        public virtual Album? Album { get; set; }
+        
+    }
+}
