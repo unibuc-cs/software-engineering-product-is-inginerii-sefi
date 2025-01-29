a0e6f409e49f0482f07b2e1a910ec2f28387add4
Maria Preda <117992883+mariapreda19@users.noreply.github.com>
2025-01-29 17:25:40 +0200

    Modelele pentru Like si Play

---
+++ FreeMusicInstantly/FreeMusicInstantly/Data/Migrations/20250129152154_mig102.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreeMusicInstantly.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig102 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Like",
+                columns: table => new
+                {
+                    LikeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SongId = table.Column<int>(type: "int", nullable: false),
+                    LikeDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Like", x => x.LikeId);
+                    table.ForeignKey(
+                        name: "FK_Like_AspNetUsers_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Like_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Play",
+                columns: table => new
+                {
+                    PlayId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SongId = table.Column<int>(type: "int", nullable: false),
+                    PlayTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Play", x => x.PlayId);
+                    table.ForeignKey(
+                        name: "FK_Play_AspNetUsers_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Play_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_SongId",
+                table: "Like",
+                column: "SongId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_UserId1",
+                table: "Like",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Play_SongId",
+                table: "Play",
+                column: "SongId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Play_UserId1",
+                table: "Play",
+                column: "UserId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Like");
+
+            migrationBuilder.DropTable(
+                name: "Play");
+        }
+    }
+}
---
+++ FreeMusicInstantly/FreeMusicInstantly/Models/Like.cs
@@ -0,0 +1,21 @@
+﻿using FreeMusicInstantly.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FreeMusicInstantly.Models
+{
+    public class Like
+    {
+        [Key]
+        public int LikeId { get; set; }
+        public int UserId { get; set; }
+        public int SongId { get; set; }
+        public DateTime LikeDate { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+        public virtual Song? Song { get; set; }
+    }
+}
